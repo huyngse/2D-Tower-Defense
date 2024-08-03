13a8f9172ae45800d9ad3271b88ff191f5496d65
huyngse <ngogiahuy775309@gmail.com>
2024-08-03 11:23:55 +0700

    Currency Text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
+    [Header("References")]
+    [SerializeField]
+    private TMP_Text currencyText;
     public TowerButton ClickedButton { get; private set; }
-    void Update() {
+    public int Currency
+    {
+        get => currency;
+        set
+        {
+            currency = value;
+            currencyText.text = value.ToString() + "<color=green>$</color>";
+        }
+    }
+
+    private int currency;
+
+    void Start()
+    {
+        Currency = 10000;
+    }
+
+    void Update()
+    {
         HandleCancel();
     }
 
-    public void PickTower(TowerButton towerButton) { 
+    public void PickTower(TowerButton towerButton)
+    {
         ClickedButton = towerButton;
         Hover.Instance.Activate(ClickedButton.Sprite);
     }
-    public void BuyTower() {
+
+    public void BuyTower()
+    {
         ClickedButton = null;
         Hover.Instance.Deativate();
     }
-    private void HandleCancel() {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
+
+    private void HandleCancel()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
             ClickedButton = null;
             Hover.Instance.Deativate();
         }
