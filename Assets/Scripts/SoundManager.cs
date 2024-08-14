e9adc7e4dd1678153c4d2a71bae244822d1f45f6
huyngse <ngogiahuy775309@gmail.com>
2024-08-14 08:19:48 +0700

    Adjusting sound volume

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -26,10 +26,43 @@ public class SoundManager : Singleton<SoundManager>
         {
             audioClips.Add(clip.name, clip);
         }
+        LoadVolume();
+        musicSlider
+            .onValueChanged
+            .AddListener(
+                delegate
+                {
+                    UpdateVolume();
+                }
+            );
+        sfxSlider
+            .onValueChanged
+            .AddListener(
+                delegate
+                {
+                    UpdateVolume();
+                }
+            );
     }
 
     public void PlayEffect(string name)
     {
         sfxSource.PlayOneShot(audioClips[name]);
+    }
+
+    public void UpdateVolume()
+    {
+        musicSource.volume = musicSlider.value;
+        sfxSource.volume = sfxSlider.value;
+        PlayerPrefs.SetFloat("music", musicSource.volume);
+        PlayerPrefs.SetFloat("SFX", sfxSource.volume);
+    }
+
+    public void LoadVolume()
+    {
+        musicSource.volume = PlayerPrefs.GetFloat("music", 0.5f);
+        sfxSource.volume = PlayerPrefs.GetFloat("SFX", 0.5f);
+        musicSlider.value = musicSource.volume;
+        sfxSlider.value = sfxSource.volume;
     }
 }
